dce2572678a703e2b7e7bbbcdf39c95f06ca006e
Yagnesh874 <142610232+Yagnesh874@users.noreply.github.com>
2023-09-19 20:15:59 +0530

    Add files via upload

---
+++ StringBufferExample2.java
@@ -0,0 +1,7 @@
+public class StringBufferExample2 {
+    public static void main(String[] args) {
+        StringBuffer obj1 = new StringBuffer("Hello");
+        obj1.insert(1,"Java");
+        System.out.println(obj1);
+    }
+}
